37d12f64888ad28c2c2948d982aa06928fc81b4a
GabrielAnjos011 <gabrielgarcia.anjos250@gmail.com>
2024-02-18 15:27:48 -0300

    Ajuste no pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { jsPDF } from "jspdf";
+import { FaCheck, FaEdit, FaFilePdf, FaTrash } from "react-icons/fa";
+import { FaPaperPlane } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
 import "jspdf-autotable";
 import "./App.css";
 
@@ -11,11 +14,11 @@ const PDFGenerator = () => {
   });
   const [items, setItems] = useState([]);
   const [itemDescription, setItemDescription] = useState("");
-  const [itemQuantity, setItemQuantity] = useState("");
+  const [itemQuantity, setItemQuantity] = useState();
   const [itemColor, setItemColor] = useState("");
   const [observations, setObservations] = useState("");
   const [totalAmount, setTotalAmount] = useState("");
-  const [date, setDate] = useState(new Date().toLocaleDateString("pt-BR"));
+  const [date, setDate] = useState(new Date().toISOString().substring(0, 10));
   const [editingIndex, setEditingIndex] = useState(-1);
 
   const handleCustomerDataChange = (e) => {
@@ -40,7 +43,7 @@ const PDFGenerator = () => {
   };
 
   const handleTotalAmountChange = (e) => {
-    const value = e.target.value.replace(/\D/g, ""); // Remove non-digits
+    const value = e.target.value.replace(/\D/g, "");
     setTotalAmount(formatCurrency(value));
   };
 
@@ -53,44 +56,35 @@ const PDFGenerator = () => {
   };
 
   const handleDateChange = (e) => {
-    setDate(e.target.value);
+    const selectedDateStr = e.target.value;
+    setDate(selectedDateStr);
   };
 
   const formatPhone = (value) => {
-    // Remove tudo o que não é dígito
     const phoneNumber = value.replace(/\D/g, "");
-
-    // Verifica se o número tem 11 dígitos
     if (phoneNumber.length === 11) {
       return phoneNumber.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
     }
-    // Caso contrário, aplica a máscara padrão
     return phoneNumber.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $2-$3");
   };
 
   const formatCnpjCpf = (value) => {
-    // Remove tudo o que não é dígito
     const cnpjCpf = value.replace(/\D/g, "");
-
-    // Verifica se o número tem 14 dígitos (CNPJ)
     if (cnpjCpf.length === 14) {
       return cnpjCpf.replace(
         /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
         "$1.$2.$3/$4-$5"
       );
-    }
-    // Verifica se o número tem 11 dígitos (CPF)
-    else if (cnpjCpf.length === 11) {
+    } else if (cnpjCpf.length === 11) {
       return cnpjCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
     }
-    // Caso contrário, retorna o valor sem formatação
     return value;
   };
 
   const addItem = () => {
     const newItem = {
       description: itemDescription,
-      quantity: parseInt(itemQuantity),
+      quantity: itemQuantity ? parseInt(itemQuantity) : "",
       color: itemColor,
     };
     setItems([...items, newItem]);
@@ -100,74 +94,93 @@ const PDFGenerator = () => {
   const generatePDF = () => {
     const doc = new jsPDF();
     const currentDate = date;
+    const [year, month, day] = currentDate.split("-");
+
+    const formattedDate = `${day}/${month}/${year}`;
 
     // Header
     doc.setFillColor(0, 152, 219);
     doc.rect(0, 0, 210, 35, "F");
-    doc.setFont("Montserrat", "bold"); // Define a família da fonte como 'helvetica' e o estilo como negrito
+    doc.setFont("Montserrat", "bold");
     doc.setFontSize(27);
     doc.setTextColor(255);
     doc.text("Vidraçaria Ambires", 18, 17);
 
     doc.setFont("Montserrat", "normal");
+
     // Data atual
     doc.setFontSize(14);
     doc.setTextColor(255);
-    doc.text(`Data: ${currentDate}`, 20, 25);
+    doc.text(`Data: ${formattedDate}`, 20, 25);
 
     // Dados fixos e dinâmicos
-    doc.setTextColor(0);
     // Adicionar os dados fixos na coluna esquerda
+    doc.setTextColor(0);
     doc.text(`Nome: Daniel Ambires da Silva`, 20, 50);
     doc.text(`CNPJ: 43.244.454/0001-10`, 20, 60);
     doc.text(`Telefone: (11) 94705-8537`, 20, 70);
 
     if (customerData.name || customerData.cnpj || customerData.phone) {
-      doc.setLineWidth(1); // Define a espessura da linha como 0.5
+      doc.setLineWidth(1);
       doc.setDrawColor(204, 204, 204);
-      doc.line(105, 45, 105, 75); // linha vertical
+      doc.line(105, 45, 105, 80);
     }
 
     // Adicionar os dados dinâmicos na coluna direita
-    {
-      customerData.name ? doc.text(`Nome: ${customerData.name}`, 120, 50) : "";
+    const yPosCustomerDataNome = 50;
+    const yPosCustomerDataCNPJ = 64;
+    const yPosCustomerDataPhone = 74;
+
+    if (customerData.name) {
+      const maxWidth = 80;
+      const customerNameLines = doc.splitTextToSize(
+        `Nome: ${customerData.name}`,
+        maxWidth
+      );
+      doc.text(customerNameLines, 120, yPosCustomerDataNome);
     }
-    {
-      customerData.cnpj
-        ? doc.text(`CNPJ/CPF: ${formatCnpjCpf(customerData.cnpj)}`, 120, 60)
-        : "";
+    if (customerData.cnpj) {
+      doc.text(
+        `CNPJ/CPF: ${formatCnpjCpf(customerData.cnpj)}`,
+        120,
+        yPosCustomerDataCNPJ
+      );
     }
-    {
-      customerData.phone
-        ? doc.text(`Telefone: ${formatPhone(customerData.phone)}`, 120, 70)
-        : "";
+    if (customerData.phone) {
+      doc.text(
+        `Telefone: ${formatPhone(customerData.phone)}`,
+        120,
+        yPosCustomerDataPhone
+      );
     }
 
     // Tabela de Itens
     const tableData = items.map((item) => [
-      item.description,
+      {
+        content: item.description,
+        styles: { valign: "top", halign: "left" },
+      },
       item.quantity,
       item.color,
     ]);
     doc.autoTable({
-      startY: 90,
+      startY: yPosCustomerDataPhone + 10,
       head: [["Descrição", "Quantidade", "Cor"]],
       body: tableData,
+      theme: "grid",
+      headStyles: {
+        fillColor: [0, 152, 219],
+      },
     });
 
     // Rodapé
-    doc.text(
-      `Observações: ${observations}`,
-      20,
-      doc.autoTable.previous.finalY + 20
-    );
-    doc.text(
-      `Valor Total: ${totalAmount}`,
-      120,
-      doc.autoTable.previous.finalY + 20
-    );
+    const startYFooter = Math.max(doc.autoTable.previous.finalY + 20);
+    if (observations) {
+      doc.text(`Observações: ${observations}`, 20, startYFooter);
+    }
+    doc.text(`Valor Total: ${totalAmount}`, 20, startYFooter + 15);
 
-    doc.save("orcamento.pdf");
+    doc.save(`orcamento${currentDate}.pdf`);
   };
 
   const editItem = (index) => {
@@ -187,7 +200,7 @@ const PDFGenerator = () => {
     };
     setItems(updatedItems);
     clearInputs();
-    setEditingIndex(-1); // Limpar o estado de edição
+    setEditingIndex(-1);
   };
 
   const cancelEdit = () => {
@@ -214,14 +227,17 @@ const PDFGenerator = () => {
       </nav>
       <div className="container">
         <h2>Data do Orçamento</h2>
-        <span>Ao não colocar a data vai como padrão a data de hoje!</span>
-        <input type="date" value={date} onChange={handleDateChange} />
+        <span className="sub-titile">
+          Se não colocar a data, como padrão é preenchido com a data de hoje!
+        </span>
+        <input type="date" value={date} onChange={handleDateChange} required />
         <h2>Dados do Cliente</h2>
         <div>
           <label>Nome:</label>
           <input
             type="text"
             name="name"
+            placeholder="Preencha o nome do cliente"
             value={customerData.name}
             onChange={handleCustomerDataChange}
           />
@@ -234,6 +250,7 @@ const PDFGenerator = () => {
             maxLength={18}
             value={formatCnpjCpf(customerData.cnpj)}
             onChange={handleCustomerDataChange}
+            placeholder="Preencha o CNPJ ou CPF do cliente"
           />
         </div>
         <div>
@@ -244,6 +261,7 @@ const PDFGenerator = () => {
             maxLength={15}
             value={formatPhone(customerData.phone)}
             onChange={handleCustomerDataChange}
+            placeholder="Preencha o telefone do cliente"
           />
         </div>
         <h2>Itens do Orçamento</h2>
@@ -269,14 +287,23 @@ const PDFGenerator = () => {
           {editingIndex === -1 ? (
             <button className="button add" onClick={addItem}>
               Adicionar Item
+              <span className="icon">
+                <FaPaperPlane />
+              </span>
             </button>
           ) : (
             <div className="box-confirm-edit">
               <button className="button save-edit" onClick={saveItem}>
                 Salvar
+                <span className="icon">
+                  <FaCheck />
+                </span>
               </button>
               <button className="button cancel-edit" onClick={cancelEdit}>
                 Cancelar
+                <span className="icon">
+                  <MdCancel />
+                </span>
               </button>
             </div>
           )}
@@ -303,12 +330,18 @@ const PDFGenerator = () => {
                       onClick={() => editItem(index)}
                     >
                       Editar
+                      <span className="icon">
+                        <FaEdit />
+                      </span>
                     </button>
                     <button
                       className="button delete"
                       onClick={() => deleteItem(index)}
                     >
                       Excluir
+                      <span className="icon">
+                        <FaTrash />
+                      </span>
                     </button>
                   </div>
                 </td>
@@ -323,6 +356,7 @@ const PDFGenerator = () => {
               className="textareaFooterObservation"
               value={observations}
               onChange={handleObservationsChange}
+              placeholder="Preencha as observações do orçamento"
             />
           </div>
           <div className="box-footer-totalAmount">
@@ -332,11 +366,15 @@ const PDFGenerator = () => {
               type="text"
               value={totalAmount}
               onChange={handleTotalAmountChange}
+              placeholder="Preencha o valor do orçamento"
             />
           </div>
         </div>
         <button className="button generate-pdf" onClick={generatePDF}>
           Gerar PDF
+          <span className="icon">
+            <FaFilePdf />
+          </span>
         </button>
       </div>
     </div>
